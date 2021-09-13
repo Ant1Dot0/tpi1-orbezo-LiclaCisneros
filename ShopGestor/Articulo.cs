1defdad88d5aa39e47e07e1e1217f31e8fcaacdc
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-09-13 11:18:28 -0300

    Implementacion de clase Articulo (molde) y ArticuloNegocio (llamado a base de datos) en Listar Articulos

---
+++ ShopGestor/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopGestor
+{
+    class Articulo
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        //public int marca { get; set; }
+        public string descripcionMarca { get; set; }
+        //public int categoria { get; set; }
+        public string descripcionCategoria { get; set; }
+        public string Url { get; set; }
+        public decimal precio { get; set; }
+    }
+}
---
+++ ShopGestor/ArticuloNegocio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ShopGestor
+{
+    class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "SELECT Articulos.Id, Codigo, Nombre, ARTICULOS.Descripcion, MARCAS.Descripcion Marca, CATEGORIAS.Descripcion Categoria,ImagenUrl, Precio FROM ARTICULOS INNER JOIN MARCAS ON ARTICULOS.IdMarca = MARCAS.Id INNER JOIN CATEGORIAS ON ARTICULOS.IdCategoria = CATEGORIAS.Id";
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.id = (int)lector["Id"];
+                    aux.codigo = (string)lector["Codigo"];
+                    aux.nombre = (string)lector["Nombre"];
+                    aux.descripcion = (string)lector["Descripcion"];
+                    //aux.marca = (int)lector["IdMarca"];
+                    aux.descripcionMarca = (string)lector["Marca"];
+                    //aux.categoria = (int)lector["IdCategoria"];
+                    aux.descripcionCategoria = (string)lector["Categoria"];
+                    aux.Url = (string)lector["imagenUrl"];
+                    aux.precio = (decimal)lector["Precio"];
+                    lista.Add(aux);
+                }
+                conexion.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
